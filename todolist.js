21894ca9076ea8b20f5e1476f8a0adc49146d830
Runal Manandhar <runalmanandhar@gmail.com>
2022-09-21 18:27:43 +0545

    Features: add todo list

--- todolist.js
+++ todolist.js
@@ -1,18 +1,40 @@
 const task= [];
 const addbtn = document.getElementById('addbtn');
 const removebtn = document.getElementById('removebtn');
-var display=document.querySelector('.display')
+var display=document.querySelector('.display');
 addbtn.addEventListener("click",(e)=>{
-    
-    const input =document.getElementById('input').value;
+    const userInput=document.getElementById('input');
+    var input =userInput.value;
     task.push(input);
-    task.forEach((val)=>{
-        let value= val;
-        display.innerHTML= value;
-        console.log(value)
-    });
-})
-        
+    display.innerHTML+=`
+    <div class='todo-list'>
+        <li class='todo-list__input'>
+        ${input}
+        </li>
+        <div class='todo-list__btn'>
+            <button class='todo-list__btn--edit' >
+                Edit
+            </button>
+            <button class='todo-list__btn--delete'>
+                Delete
+            </button>
+        </div>
+    </div>
+     ` ;
+     userInput.value='';
+});
+
+const editBtn=document.querySelector('.todo-list__btn--edit');
+editBtn.addEventListener('click',e=>{
+    console.log('clicked');
+});
+
+
+
+const displayTodoList=()=>{
+
+    
+}
        
         
    
